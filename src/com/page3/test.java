fdf7b68f5295b3a05e6751237acc391b64480474
xuezhigang <786427910@qq.com>
2020-01-19 18:25:18 +0800

    关系运算符的练习5和6

--- src/com/page3/test.java
+++ src/com/page3/test.java
@@ -3,7 +3,18 @@ package com.page3;
 class Tank {
     float levle;
 }
+class Dog{
+    String name;
+    String says;
+    Dog(String name,String says){
+        this.name=name;
+        this.says=says;
 
+    }
+    public void notcie(){
+        System.out.println(name+"的叫声为"+says);
+    }
+}
 public class test {
     public static void main(String[] args) {
         /*Tank t1 = new Tank();
@@ -20,12 +31,20 @@ public class test {
         b=a++;
         System.out.println(b);*/
        //3.7关系运算符 ==和！=也应用在对象，但是对象比较的是对象的引用，如果要比较内容，就要用对象的equals，基本类型用== 和！=即可
-        Integer n1=new Integer(47);
+     /*   Integer n1=new Integer(47);
         Integer n2=new Integer(47);
         System.out.println(n1==n2);
         System.out.println(n1!=n2);
         System.out.println(n1.equals(n2));
-
+*/
+     //练习5
+     Dog d1=new Dog("spot","Ruff!");
+     d1.notcie();
+     Dog d2=new Dog("scruffy","Wurf!");
+     d2.notcie();
+     //练习6
+        System.out.println(d1==d2);
+        System.out.println(d1.equals(d2));
 }
 }
 
